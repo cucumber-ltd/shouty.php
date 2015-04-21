1b04fb918d6957392f905dcd5936a3a050112707
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2015-04-21 16:37:59 +0100

    2nd scenario passing now

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -7,17 +7,72 @@ use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
 use PHPUnit_Framework_Assert as PHPUnit;
 
-class Shouty {
-    public function personIsIn($personName, $location) {
+class Network {
+    private $people = [];
 
+    public function subscribe($person) {
+        array_push($this->people, $person);
+    }
+
+    public function broadcast($message) {
+        foreach($this->people as $listener) {
+          $listener->hear($message);
+        }
+    }
+}
+
+class Shouty {
+    private $people = [];
+    private $network;
+
+    public function __construct() {
+        $this->network = new Network();
+    }
+
+    public function personIsIn($personName, $location) {
+        $this->person($personName)->isIn($location);
     }
 
     public function personShouts($personName, $message) {
-
+        $this->person($personName)->shout($message);
     }
 
     public function heardBy($personName) {
-        return [];
+        return $this->person($personName)->messagesHeard();
+    }
+
+    private function person($personName) {
+        if(array_key_exists($personName, $this->people)) {
+            return $this->people[$personName];
+        }
+        return $this->people[$personName] = new Person($this->network);
+    }
+}
+
+class Person {
+    private $location;
+    private $network;
+    private $messagesHeard = [];
+
+    public function __construct($network) {
+        $this->network = $network;
+        $network->subscribe($this);
+    }
+
+    public function isIn($location) {
+        $this->location = $location;
+    }
+
+    public function shout($message) {
+        $this->network->broadcast($message);
+    }
+
+    public function hear($message) {
+        array_push($this->messagesHeard, $message);
+    }
+
+    public function messagesHeard() {
+        return $this->messagesHeard;
     }
 }
 
@@ -52,7 +107,7 @@ class FeatureContext implements Context, SnippetAcceptingContext
     }
 
     /**
-     * @Given /(\w+) is in (.+)/
+     * @Given /^(\w+) is in (.+)$/
      */
     public function personIsInLocation($personName, $locationName)
     {
@@ -61,7 +116,7 @@ class FeatureContext implements Context, SnippetAcceptingContext
     }
 
     /**
-     * @When /(\w+) shouts/
+     * @When /^(\w+) shouts$/
      */
     public function personShouts($personName)
     {
@@ -69,11 +124,26 @@ class FeatureContext implements Context, SnippetAcceptingContext
     }
 
     /**
-     * @Then /(\w+) should not hear anything/
+     * @When /^(\w+) shouts "(.*)"$/
+     */
+    public function personShoutsMessage($personName, $message)
+    {
+        $this->shouty->personShouts($personName, $message);
+    }
+
+    /**
+     * @Then /^(\w+) should not hear anything$/
      */
     public function personShouldNotHearAnything($personName)
     {
         PHPUnit::assertEquals([], $this->shouty->heardBy($personName));
     }
 
+    /**
+     * @Then /^(\w+) should hear "(.*)"$/
+     */
+    public function personShouldHearMessage($personName, $expectedMessage)
+    {
+        PHPUnit::assertEquals([$expectedMessage], $this->shouty->heardBy($personName));
+    }
 }
