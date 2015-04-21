1d449fe5c3a1e55f70e3c1a968a037f7c5307e63
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2015-04-21 15:49:54 +0100

    One scenario passing

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -2,14 +2,33 @@
 
 use Behat\Behat\Context\Context;
 use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
+use PHPUnit_Framework_Assert as PHPUnit;
+
+class Shouty {
+    public function personIsIn($personName, $location) {
+
+    }
+
+    public function personShouts($personName, $message) {
+
+    }
+
+    public function heardBy($personName) {
+        return [];
+    }
+}
 
 /**
  * Defines application features from the specific context.
  */
 class FeatureContext implements Context, SnippetAcceptingContext
 {
+    private $locations = [];
+    private $shouty;
+
     /**
      * Initializes context.
      *
@@ -19,5 +38,42 @@ class FeatureContext implements Context, SnippetAcceptingContext
      */
     public function __construct()
     {
+        $this->shouty = new Shouty();
     }
+
+    /**
+     * @Given the following locations:
+     */
+    public function theFollowingLocations(TableNode $locations)
+    {
+        foreach ($locations->getHash() as $location) {
+            $this->locations[$location['name']] = [doubleval($location['lat']), doubleval($location['lon'])];
+        }
+    }
+
+    /**
+     * @Given /(\w+) is in (.+)/
+     */
+    public function personIsInLocation($personName, $locationName)
+    {
+        $location = $this->locations[$locationName];
+        $this->shouty->personIsIn($personName, $location);
+    }
+
+    /**
+     * @When /(\w+) shouts/
+     */
+    public function personShouts($personName)
+    {
+        $this->shouty->personShouts($personName, 'Can you hear me?');
+    }
+
+    /**
+     * @Then /(\w+) should not hear anything/
+     */
+    public function personShouldNotHearAnything($personName)
+    {
+        PHPUnit::assertEquals([], $this->shouty->heardBy($personName));
+    }
+
 }
--- features/hear_shout.feature
+++ features/hear_shout.feature
@@ -1,5 +1,43 @@
 Feature: Hear shout
 
-  A Description goes here
+  Rules:
+  - Must be within 1 km
+  - Displayed in order of creation
+  - Network and geo location must be enabled
 
-  Scenario: What should it do?
+  Notes:
+  - Display error when no geo or network
+
+  Assumption:
+  - Everybody has geo location and network
+
+  Questions:
+  - Should people hear their own messages?
+
+  Background:
+    Given the following locations:
+      | name                   | lat       | lon      |
+      | the Norwich Castle     | 52.682729 | 1.296386 |
+      | Washington DC          | 38.8951   | -77.0367 |
+      | the Bell Hotel Norwich | 52.6725   | 1.29517  |
+
+  Scenario: Phil can't hear Jeff who is far away
+    Given Jeff is in the Norwich Castle
+    And Phil is in Washington DC
+    When Jeff shouts
+    Then Phil should not hear anything
+
+  Scenario: Phil can hear Sally who is within range
+    Given Sally is in the Norwich Castle
+    And Phil is in the Bell Hotel Norwich
+    When Sally shouts "You around Phil?"
+    Then Phil should hear "You around Phil?"
+
+  Scenario: Phil hears Jeff's shout after Sally's shout
+    Given Phil is around
+    When Sally shouts "one"
+    And Jeff shouts "two"
+    Then Phil should hear:
+      | message |
+      | one     |
+      | two     |
