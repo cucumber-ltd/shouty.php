a11007234d37b81c67f313a8b3f0cea2c7c25fec
Steve Tooke <tooky@cucumber.io>
2016-12-08 14:22:04 +0000

    Get to green

--- src/Shouty/Coordinate.php
+++ src/Shouty/Coordinate.php
@@ -2,16 +2,22 @@
 
 namespace Shouty;
 
-class Coordinate {
-  private $x;
-  private $y;
+class Coordinate
+{
+    private $x;
+    private $y;
 
-  public function __construct($xCoord, $yCoord) {
-    $this->x = $xCoord;
-    $this->y = $yCoord;
-  }
+    public function __construct($xCoord, $yCoord)
+    {
+        $this->x = $xCoord;
+        $this->y = $yCoord;
+    }
 
-  public function distanceFrom($other) {
-    return 0;
-  }
+    public function distanceFrom($other)
+    {
+        $xSquared = pow($other->x - $this->x, 2);
+        $ySquared = pow($other->y - $this->y, 2);
+
+        return sqrt($xSquared + $ySquared);
+    }
 }
--- tests/Shouty/CoordinateTest.php
+++ tests/Shouty/CoordinateTest.php
@@ -7,9 +7,18 @@ use Shouty\Coordinate;
 
 class CoordinateTest extends TestCase
 {
-  public function testItCalculatesTheDistanceFromAnotherCoordinateAlongXAxis() {
+    public function testItCalculatesTheDistanceFromAnotherCoordinateAlongXAxis()
+    {
         $a = new Coordinate(0, 0);
         $b = new Coordinate(1000, 0);
         $this->assertEquals(1000, $a->distanceFrom($b));
     }
+
+    public function testItCalculatesTheDistanceFromAnotherCoordinateAlongYAxis()
+    {
+        $a = new Coordinate(0, 200);
+        $b = new Coordinate(0, 500);
+        $this->assertEquals(300, $a->distanceFrom($b));
+    }
+
 }
